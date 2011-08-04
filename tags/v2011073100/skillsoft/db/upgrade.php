2ed3f62872c4bc299aba6766754f250f45ecaa93
martinholden1972 <martinholden1972@5611cade-e957-11de-9c2a-cb0f5ba72f9d>
2011-08-04 16:19:00 +0000

    Version: 2011073100 1.9.x build incorporating support for: * Custom Report retrieval of usage data when using "Track to OLSA" * Multiple Completions support in "Track to LMS" and "Track to OLSA" when using Custom Report mode. * Support for accessing OLSA Soap Services via Proxy, utilising CURL * Support for use of OLSA Seamless Login to deliver user into SkillPort to access content.
    This is the only version of the module for Moodle 1.9.x that can be updated to the Moodle 2.x module version. See http://code.google.com/p/moodle2-skillsoft-activity/

---
+++ tags/v2011073100/skillsoft/db/upgrade.php
@@ -0,0 +1,173 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This file keeps track of upgrades to the olsasso module
+ *
+ * Sometimes, changes between versions involve alterations to database
+ * structures and other major things that may break installations. The upgrade
+ * function in this file will attempt to perform all the necessary actions to
+ * upgrade your older installtion to the current version. If there's something
+ * it cannot do itself, it will tell you what you need to do.  The commands in
+ * here will all be database-neutral, using the functions defined in
+ * lib/ddllib.php
+ *
+ * @package   mod-olsa
+ * @author 	  Martin Holden
+ * @copyright 2009-2011 Martin Holden
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * xmldb_skillsoft_upgrade
+ *
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_skillsoft_upgrade($oldversion=0) {
+
+	global $CFG, $THEME, $db;
+
+	$result = true;
+
+    if ($result && $oldversion < 2010040700) {
+    /// Define field username to be added to skillsoft_tdr
+        $table = new XMLDBTable('skillsoft_tdr');
+        $field = new XMLDBField('username');
+        $field->setAttributes(XMLDB_TYPE_CHAR, '255', null, null, null, null, null, 'null', 'userid');
+
+    /// Launch add field username
+        $result = $result && add_field($table, $field);
+    }
+
+    if ($result && $oldversion < 2011011200) {
+
+    /// Define table skillsoft_report_track to be created
+        $table = new XMLDBTable('skillsoft_report_track');
+
+    /// Adding fields to table skillsoft_report_track
+        $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
+        $table->addFieldInfo('startdate', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('enddate', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('handle', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('url', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('localpath', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('polled', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('downloaded', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('imported', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('processed', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('timerequested', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('timepolled', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('timedownloaded', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('timeimported', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+        $table->addFieldInfo('timeprocessed', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+
+    /// Adding keys to table skillsoft_report_track
+        $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+    /// Launch create table for skillsoft_report_track
+        $result = $result && create_table($table);
+    }
+
+    if ($result && $oldversion < 2011011200) {
+
+    /// Define table skillsoft_report_results to be created
+        $table = new XMLDBTable('skillsoft_report_results');
+
+    /// Adding fields to table skillsoft_report_results
+        $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
+        $table->addFieldInfo('loginname', XMLDB_TYPE_CHAR, '255', null, null, null, null, null, 'null');
+        $table->addFieldInfo('lastname', XMLDB_TYPE_CHAR, '255', null, null, null, null, null, 'null');
+        $table->addFieldInfo('firstname', XMLDB_TYPE_CHAR, '255', null, null, null, null, null, 'null');
+        $table->addFieldInfo('assetid', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('firstaccessdate', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0');
+        $table->addFieldInfo('lastaccessdate', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0');
+        $table->addFieldInfo('completeddate', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, '0');
+        $table->addFieldInfo('firstscore', XMLDB_TYPE_NUMBER, '10, 2', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('currentscore', XMLDB_TYPE_NUMBER, '10, 2', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('bestscore', XMLDB_TYPE_NUMBER, '10, 2', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('lessonstatus', XMLDB_TYPE_CHAR, '30', null, null, null, null, null, null);
+        $table->addFieldInfo('duration', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('accesscount', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('userid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('processed', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0');
+
+    /// Adding keys to table skillsoft_report_results
+        $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+    /// Adding indexes to table skillsoft_report_results
+        $table->addIndexInfo('loginname-assetid', XMLDB_INDEX_UNIQUE, array('loginname', 'assetid'));
+
+    /// Launch create table for skillsoft_report_results
+        $result = $result && create_table($table);
+    }
+
+    if ($result && $oldversion = 2011011200) {
+        /// Define field username to be added to skillsoft_tdr
+        $table = new XMLDBTable('skillsoft_report_track');
+        $field = new XMLDBField('polled');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'localpath');
+	    /// Launch add field polled
+        $result = $result && add_field($table, $field);
+
+        $field = new XMLDBField('imported');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'downloaded');
+	    /// Launch add field polled
+        $result = $result && add_field($table, $field);
+
+        $field = new XMLDBField('timepolled');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'timerequested');
+        $result = $result && add_field($table, $field);
+
+        $field = new XMLDBField('timeimported');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'timedownloaded');
+        $result = $result && add_field($table, $field);
+    }
+    
+	if ($result && $oldversion < 2011071400) {
+
+    	/// Define index loginname-assetid (unique) to be dropped form skillsoft_report_results
+        $table = new XMLDBTable('skillsoft_report_results');
+        $index = new XMLDBIndex('loginname-assetid');
+        $index->setAttributes(XMLDB_INDEX_UNIQUE, array('loginname', 'assetid'));
+
+    	/// Launch drop index loginname-assetid
+        $result = $result && drop_index($table, $index);
+        
+        /// Define index loginname-assetid (unique) to be added to skillsoft_report_results
+        $table = new XMLDBTable('skillsoft_report_results');
+        $index = new XMLDBIndex('loginname-assetid-firstaccessdate');
+        $index->setAttributes(XMLDB_INDEX_UNIQUE, array('loginname', 'assetid', 'firstaccessdate'));
+
+    	/// Launch add index loginname-assetid
+        $result = $result && add_index($table, $index);
+        
+        
+        // Define field username to be added to skillsoft_report_results to track attempt number
+        $table = new XMLDBTable('skillsoft_report_results');
+        $field = new XMLDBField('attempt');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'processed');
+	    /// Launch add field polled
+        $result = $result && add_field($table, $field);
+        
+    }
+    
+	return $result;
+}
---
+++ tags/v2011073100/skillsoft/index.php
@@ -0,0 +1,112 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * This is a one-line short description of the file
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package   mod-skillsoft
+ * @author    Martin Holden
+ * @copyright 2009-20111 Martin Holden
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/// Replace skillsoft with the name of your module and remove this line
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+$id = required_param('id', PARAM_INT);   // course id
+
+if (!empty($id)) {
+	if (! $course = get_record('course', 'id', $id)) {
+		error('Course ID is incorrect');
+	}
+} else {
+	error('A required parameter is missing');
+}
+
+require_course_login($course);
+
+add_to_log($course->id, 'skillsoft', 'view all', 'index.php?id='.$course->id, 'View all SkillSoft Assets');
+
+//Retrieve the localisation strings
+
+$strskillsoft = get_string('modulename', 'skillsoft');
+$strskillsofts = get_string('modulenameplural', 'skillsoft');
+$strweek = get_string('week');
+$strtopic = get_string('topic');
+$strskillsoftid = get_string('skillsoft_assetid', 'skillsoft');
+$strskillsoftsummary = get_string('skillsoft_summary', 'skillsoft');
+$strlastmodified = get_string('lastmodified');
+$strreport = get_string('skillsoft_report','skillsoft');
+$strname = get_string('skillsoft_name','skillsoft');
+
+//Navigation Links
+$navlinks = array();
+$navlinks[] = array('name' => $strskillsofts, 'link' => '', 'type' => 'activity');
+$navigation = build_navigation($navlinks);
+
+print_header_simple($strskillsofts, '', $navigation, '', '', true, '', navmenu($course));
+
+if (! $skillsofts = get_all_instances_in_course('skillsoft', $course)) {
+	notice(get_string('thereareno', 'moodle', $strskillsofts), '../../course/view.php?id=$course->id');
+	exit;
+}
+
+if ($course->format == 'weeks') {
+	$table->head  = array ($strweek, $strskillsoftid, $strname, $strskillsoftsummary, $strreport);
+	$table->align = array ('center', 'left', 'left','left', 'left');
+} else if ($course->format == 'topics') {
+	$table->head  = array ($strtopic, $strskillsoftid, $strname, $strskillsoftsummary, $strreport);
+	$table->align = array ('center', 'left', 'left','left', 'left');
+} else {
+	$table->head  = array ($strlastmodified, $strskillsoftid, $strname, $strskillsoftsummary, $strreport);
+	$table->align = array ('left', 'left', 'left','left', 'left');
+}
+
+foreach ($skillsofts as $skillsoft) {
+	$context = get_context_instance(CONTEXT_MODULE,$skillsoft->coursemodule);
+	$tt = '';
+	if ($course->format == 'weeks' or $course->format == 'topics') {
+		if ($skillsoft->section) {
+			$tt = $skillsoft->section;
+		}
+	} else {
+		$tt = userdate($skillsoft->timemodified);
+	}
+
+    $reportshow = '&nbsp;';
+	if (has_capability('mod/skillsoft:viewreport', $context)) {
+		$reportshow = '<a href="report.php?id='.$skillsoft->id.'">'.get_string('skillsoft_viewallreport','skillsoft').'</a></div>';
+   	}
+
+	if (!$skillsoft->visible) {
+		//Show dimmed if the mod is hidden
+		$table->data[] = array ($tt, $skillsoft->assetid, '<a class="dimmed" href="view.php?id='.$skillsoft->coursemodule.'">'.format_string($skillsoft->name).'</a>', $skillsoft->summary, $reportshow);
+	} else {
+		//Show normal if the mod is visible
+		$table->data[] = array ($tt, $skillsoft->assetid, '<a href="view.php?id='.$skillsoft->coursemodule.'">'.format_string($skillsoft->name).'</a>', $skillsoft->summary, $reportshow);
+	}
+}
+echo '<br />';
+print_table($table);
+print_footer($course);
+?>
